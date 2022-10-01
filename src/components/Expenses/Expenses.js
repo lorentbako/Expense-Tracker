8d932283d40fd29061bd7344e036d5752267cea8
lorentbako <lorentbako@gmail.com>
2022-10-01 12:02:05 +0200

    Full app finished where you can add an expense, and filter it by year, also with a chart bar included

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import Card from "../UI/Card.js";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter.js";
+import { useState } from "react";
+import ExpensesList from "./ExpensesList.js";
+import ExpensesChart from "./ExpensesChart.js";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2022");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <li>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </li>
+  );
+}
+
+export default Expenses;
